40f8fcc10cfd0dd6aaff3f46863be1b6b0d8d788
iziKomSD <sparkden4ik2@gmail.com>
2024-08-13 12:44:55 +0300

    current lesson 20

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Button from './components/Button/Button'
+
+function App() {
+  // return React.createElement('div', {}, 'Project')
+  return (
+    <>
+      <h1>Heading</h1>
+      <p>Some text after heading</p>
+      <Button />
+    </>
+  )
+}
+
+export default App
