1cf14ab9b3769b20349296bc0cd1efb67e5c2601
iziKomSD <sparkden4ik2@gmail.com>
2024-08-31 17:40:10 +0300

    current lesson 28 finished

--- src/components/JournalAddButton/JournalAddButton.jsx
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -2,7 +2,7 @@ import CardButton from '../CardButton/CardButton'
 import './JournalAddButton.css'
 
 function JournalAddButton() {
-  return <CardButton>New Memory</CardButton>
+  return <CardButton className="journal-add">New Memory</CardButton>
 }
 
 export default JournalAddButton
